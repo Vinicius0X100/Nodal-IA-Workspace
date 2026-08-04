dc6c4812c58ed3d41e08006ad7203e8fc233f48d
Vinicius Aquino <vinicius.aquino@sacratech.com>
2026-08-03 21:09:01 -0300

    Ajuste do envio e adicao de usuario

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Illuminate\Support\Facades\Mail::extend('brevo', function (array $config = []) {
+            return new \Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoApiTransport(
+                config('mail.mailers.brevo.key')
+            );
+        });
     }
 }
