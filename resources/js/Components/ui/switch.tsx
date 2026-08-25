5ead9804268d4aba2a9174a96d56057731bc7436
Vinicius Aquino <vinicius.aquino@sacratech.com>
2026-08-25 17:11:16 -0300

    Ajuste de switch jsx

--- resources/js/Components/ui/switch.tsx
+++ resources/js/Components/ui/switch.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
+import { Root, Thumb } from "@radix-ui/react-switch"
 
 import { cn } from "@/lib/utils"
 
 const Switch = React.forwardRef<
-  React.ElementRef<typeof SwitchPrimitives.Root>,
-  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+  React.ElementRef<typeof Root>,
+  React.ComponentPropsWithoutRef<typeof Root>
 >(({ className, ...props }, ref) => (
-  <SwitchPrimitives.Root
+  <Root
     className={cn(
       "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-all duration-200 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-40 data-[state=checked]:bg-primary-600 data-[state=unchecked]:bg-neutral-300 hover:data-[state=unchecked]:bg-neutral-400 active:scale-95",
       className
@@ -15,13 +15,13 @@ const Switch = React.forwardRef<
     {...props}
     ref={ref}
   >
-    <SwitchPrimitives.Thumb
+    <Thumb
       className={cn(
         "pointer-events-none block h-5 w-5 rounded-full bg-white shadow-md ring-0 transition-transform duration-200 ease-in-out data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
       )}
     />
-  </SwitchPrimitives.Root>
+  </Root>
 ))
-Switch.displayName = SwitchPrimitives.Root.displayName
+Switch.displayName = Root.displayName
 
 export { Switch }
